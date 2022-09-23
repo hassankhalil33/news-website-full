8a9b4129f2c01a119aa860fd15f250f4b5c05c8c
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-23 14:55:29 +0300

    Added Get Data Function

--- backend/retrieve_data.php
+++ backend/retrieve_data.php
@@ -2,4 +2,20 @@
 
 include("connection.php");
 
+function getData() {
+    $query = $mysql -> prepare(
+    "SELECT * FROM data");
+
+    $query -> execute();
+    $array = $query -> getResult();
+
+    $response = [];
+
+    while($i = $array -> fetch_assoc()) {
+        $response[] = $i;
+    };
+
+    return $response
+}
+
 ?>
