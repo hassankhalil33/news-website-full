2076f1942eb8e150ab4e67ecb4f0b86652f00638
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-23 15:27:31 +0300

    Tested Get JSON Ajax

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,5 @@
-$.get("http://localhost/fswo5/news/retrieve_data.php", function (data) {
-    console.log(data);
+$.getJSON("http://localhost/fswo5/news/retrieve_data.php", function (data) {
+    data.forEach(element => {
+        console.log(element);
+    });
 });
